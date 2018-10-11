2022018ff7d31649ee5349bd96eaec7108919e55
马静怡 <472382917@qq.com>
2018-10-11 17:09:53 -0400

    connect and disconnect

--- server/server.js
+++ server/server.js
@@ -4,16 +4,30 @@
 const path = require('path');
 const publicPath = path.join(__dirname,'../public');
 const port =process.env.PORT || 3000;
+const socketIO = require('socket.io');
+const http = require('http');
 
 const express = require('express');
 
 
 
 var app = express();
+var server =http.createServer(app);
+var io =socketIO(server);
+
+io.on('connection',(socket)=>{
+    console.log('new user connected!!!');
+
+    socket.on('disconnect',()=>{
+        console.log('new user disconnected!')
+    });
+});
+
+
 
 
 app.use(express.static(publicPath));
-app.listen(port,()=>{
+server.listen(port,()=>{
     console.log(`Server is up on port ${port}!!!`)
 });
 
