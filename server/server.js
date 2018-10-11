4e44257d4eeec3d1e7476c1953e77366cc3865d1
马静怡 <472382917@qq.com>
2018-10-11 14:53:29 -0400

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,19 @@
+//create new express app
+// app.listen to start up
+
+const path = require('path');
+const publicPath = path.join(__dirname,'../public');
+const port =process.env.PORT || 3000;
+
+const express = require('express');
+
+
+
+var app = express();
+
+
+app.use(express.static(publicPath));
+app.listen(port,()=>{
+    console.log(`Server is up on port ${port}!!!`)
+});
+
